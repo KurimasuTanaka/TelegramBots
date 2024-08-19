7037f4e96726984b54c09b62d47e28a154747d42
Kurimasu Tanaka <kamyshek185@gmail.com>
2024-08-19 16:44:15 +0300

    Remove ignored files

--- StudyBot/Services/PollingService.cs
+++
@@ -1,6 +0,0 @@
-
-using Microsoft.Extensions.Logging;
-using StudyBot.Abstract;
-
-public class PollingService(IServiceProvider serviceProvider, ILogger<PollingService> logger) 
-: PollingServiceBase<ReceiverService>(serviceProvider, logger);
